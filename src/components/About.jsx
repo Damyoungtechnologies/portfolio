18061b6c7b7d4eff8775e46fddb7c5cc79ec22c3
damyoungtechnologies <damyounggraphics@gmail.com>
2025-08-28 00:10:55 +0100

    trying the image under the hero page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             <img
               src={my_portrait}
               alt="About me"
-              className="!h-[20] w-auto md:w-[50] object-contain rounded-sm"
+              className="h-[20] w-auto md:h-[50] object-contain rounded-sm"
             />
           </div>
           <AboutTabs />
