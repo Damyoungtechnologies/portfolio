8d7fb03f60c972b13317270273237f1660600e32
damyoungtechnologies <damyounggraphics@gmail.com>
2025-08-31 03:29:54 +0100

    hero text adjustment again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
           <h1 className="text-[4rem] font-medium text-l md:text-xl lg:text-2xl mt-[15px]">
             I am Samuel Ayodeji
           </h1>
-          <h1 className="text-[0.8rem] md:text-[1rem] font-light flex gap-2">
+          <h1 className="text-[0.5rem] md:text-[1rem] font-light flex gap-2">
             I love{"  "}
             <span>
               <Typewriter
